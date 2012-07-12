c7020a09253096a740a0fe29857a06806f65647c
pikislabis <josparbar@gmail.com>
2012-07-12 12:35:59 +0200

    Admin user enabled by seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(:name => 'Administrador', :email => 'tickethq.rails@gmail.com', :login => 'admin', :password => 'administrador', :password_confirmation => 'administrador', :admin => true, :enabled => true) if User.all.blank?
+if User.all.blank?
+	user = User.create(:name => 'Administrador', :email => 'tickethq.rails@gmail.com', :login => 'admin', :password => 'administrador', :password_confirmation => 'administrador', :admin => true)
+	user.enabled = true
+	user.save
+end
