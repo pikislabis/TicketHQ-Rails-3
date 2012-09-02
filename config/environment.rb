37afd7aceddc5eb67f5240030b304f4a9d1e7700
José Antonio Parra <josparbar@gmail.com>
2012-09-02 17:31:16 +0200

    Minor changes

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,6 @@
 require File.expand_path('../application', __FILE__)
 
 Encoding.default_external = Encoding::UTF_8
-Encoding.default_internal = Encoding::UTF_8
 
 # Initialize the rails application
 TicketHQ::Application.initialize!
